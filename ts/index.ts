0ca48dda5cb86b3a1369946c36babb541e8a3426
Käkäsh <77887301+MarcoZ05@users.noreply.github.com>
2023-03-16 22:11:21 +0100

    w

--- ts/index.ts
+++ ts/index.ts
@@ -4,13 +4,13 @@ import { Worktop } from "./objects.js";
 const canvas = document.getElementById("game") as HTMLCanvasElement;
 const game = new Game(canvas, { w: 1920, h: 1080 });
 
-// game.addPlayer(
-//   { x: 500, y: 500 },
-//   { w: 150, h: 150 },
-//   "aqua",
-//   "CopiedByKakashi",
-//   true
-// );
+game.addPlayer(
+  { x: 500, y: 500 },
+  { w: 150, h: 150 },
+  "aqua",
+  "CopiedByKakashi",
+  true
+);
 
 game.addGameObject(
   new Worktop({
